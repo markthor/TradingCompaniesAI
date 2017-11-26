239d2fa972fc11f407514f1087eb979f52b5728f
Andreas Precht Poulsen <andreas.p.poulsen@gmail.com>
2017-11-26 10:32:07 +0100

    Adds function to sell a random resource

--- src/game/Player.kt
+++ src/game/Player.kt
@@ -8,7 +8,7 @@ class Player {
 
 	var cash = 6
 	private val resources = HashMap<Resources, Int>()
-	val rng = Random()
+	private val rng = Random()
 
 	init {
 		resources.put(Resources.ENERGY, 2)
@@ -27,7 +27,18 @@ class Player {
 					break
 				}
 			}
+		} else {
+			throw IllegalStateException("Insufficient funds")
 		}
+	}
+
+	fun sellRandomResource() {
+		val sellableResources = resources.filter { it.value > 0 && Marketplace.canSell(it.key) }
+		if (sellableResources.isEmpty()) {
+			throw IllegalStateException("Nothing to sell")
+		}
+		val entry = sellableResources.entries.toTypedArray()[rng.nextInt(sellableResources.size)]
+		Marketplace.sell(entry.key, this)
 	}
 
 	private fun increment(resource: Resources) {
